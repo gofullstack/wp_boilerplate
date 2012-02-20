86e9648daa65112f70f49c8f065bb060af6ea172
Nathan L Smith <nlloyds@gmail.com>
2012-02-20 08:58:13 -0600

    uncommitted changes

--- js/plugins.js
+++ js/plugins.js
@@ -1,3 +1,24 @@
+// Uses the value attribute if placeholder
+// attribute doesn't exist on search form
+(function placeholders() {
+    if (!Modernizr.input.placeholder) {
+        $('#s')
+            .val('Search')
+            .focus(function () {
+                var t = $(this).data('initialValue');
+                if (!t) {
+                    $(this).data('initialValue', this.value).val('');
+                } else if (this.value === t) {
+                    this.value = '';
+                }
+            })
+            .blur(function () {
+                if (this.value === '') {
+                    this.value = $(this).data('initialValue');
+                }
+            });
+    }
+}());
 
 // usage: log('inside coolFunc', this, arguments);
 // paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
--- js/script.js
+++ js/script.js
@@ -2,27 +2,7 @@
  *
  */
 
-// Uses the value attribute if placeholder
-// attribute doesn't exist on search form
-(function placeholders() {
-    if (!Modernizr.input.placeholder) {
-        $('#s')
-            .val('Search')
-            .focus(function () {
-                var t = $(this).data('initialValue');
-                if (!t) {
-                    $(this).data('initialValue', this.value).val('');
-                } else if (this.value === t) {
-                    this.value = '';
-                }
-            })
-            .blur(function () {
-                if (this.value === '') {
-                    this.value = $(this).data('initialValue');
-                }
-            });
-    }
-}());
+
 
 
 
